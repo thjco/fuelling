fcea6664a8b00a20345c121fd199868b721ed678
thjco <github@colla.de>
2022-10-05 21:10:48 +0200

    Have tabs for entry, analysis and data.

--- fuelling.py
+++ fuelling.py
@@ -25,75 +25,79 @@ ensure_tables(conn)
 entries = select_all_entries(conn)
 
 
-def entry_input(data):
-    with st.form("input"):
+tab_entry, tab_analysis, tab_data = st.tabs(["Eingabe", "Auswertung", "Daten"])
 
-        # tankdatum und uhrzeit
-        fdate_date = st.date_input("Tankdatum", value=data.fdate.date()) # value="2022/10/02") #
-        fdate_time = st.time_input("Tankzeit", value=data.fdate.time())
-        fdate = datetime.combine(fdate_date, fdate_time)
-        fdate = fdate.replace(microsecond=0)
-        fdate = str(fdate)
+with tab_entry:
+    def entry_input(data):
+        with st.form("input"):
 
-        # menge
-        quantity = st.number_input("Menge", min_value=0.0, step=0.01, value=data.quantity)
+            # tankdatum und uhrzeit
+            fdate_date = st.date_input("Tankdatum", value=data.fdate.date()) # value="2022/10/02") #
+            fdate_time = st.time_input("Tankzeit", value=data.fdate.time())
+            fdate = datetime.combine(fdate_date, fdate_time)
+            fdate = fdate.replace(microsecond=0)
+            fdate = str(fdate)
 
-        # vollgetankt, teilbetankung
-        full = st.radio("Füllstand", ("vollgetankt", "teilbetankt"), index=(1-data.full), horizontal=True)
+            # menge
+            quantity = st.number_input("Menge", min_value=0.0, step=0.01, value=data.quantity)
 
-        # preis
-        price = st.number_input("Preis", min_value=0.0, step=0.01, value=data.price)
+            # vollgetankt, teilbetankung
+            full = st.radio("Füllstand", ("vollgetankt", "teilbetankt"), index=(1-data.full), horizontal=True)
 
-        # km-Stand
-        mileage = st.number_input("km-Stand", min_value=0, value=data.mileage)
+            # preis
+            price = st.number_input("Preis", min_value=0.0, step=0.01, value=data.price)
 
-        # Station
-        station = st.text_input("Station", value=data.station, max_chars=32)
+            # km-Stand
+            mileage = st.number_input("km-Stand", min_value=0, value=data.mileage)
 
-        # Verbrauch berechnen
-        evaluate = st.radio("Konsistenz", ("Verbrauch berechnen", "Keine Auswertung"), index=(1-data.evaluate), horizontal=True)
+            # Station
+            station = st.text_input("Station", value=data.station, max_chars=32)
 
-        # Kommentar
-        comment = st.text_area("Kommentar", value=data.comment, max_chars=128)
+            # Verbrauch berechnen
+            evaluate = st.radio("Konsistenz", ("Verbrauch berechnen", "Keine Auswertung"), index=(1-data.evaluate), horizontal=True)
 
-        submitted = st.form_submit_button("Absenden")
-        if submitted:
-            # TODO Close connection at this point?
-            data = { "fdate": fdate, "quantity": quantity, "full": full=="vollgetankt", "price": price,
-                    "mileage": mileage, "station": station, "evaluate": evaluate=="Verbrauch berechnen", "comment": comment }
-            return pd.Series(data=data)
-        else:
-            return None
+            # Kommentar
+            comment = st.text_area("Kommentar", value=data.comment, max_chars=128)
 
+            submitted = st.form_submit_button("Absenden")
+            if submitted:
+                # TODO Close connection at this point?
+                data = { "fdate": fdate, "quantity": quantity, "full": full=="vollgetankt", "price": price,
+                        "mileage": mileage, "station": station, "evaluate": evaluate=="Verbrauch berechnen", "comment": comment }
+                return pd.Series(data=data)
+            else:
+                return None
 
-default_entry = get_default_entry()
-entry = entry_input(default_entry)
+    default_entry = get_default_entry()
+    entry = entry_input(default_entry)
 
-if isinstance(entry, pd.Series):
-    create_entry(conn, entry)
-    entries = select_all_entries(conn)
+    if isinstance(entry, pd.Series):
+        create_entry(conn, entry)
+        entries = select_all_entries(conn)
 
-insights = get_insights(entries)
+with tab_analysis:
+    insights = get_insights(entries)
 
-consumption = insights[["fdate", "consumption"]].dropna()
-if consumption.shape[0] > 0:
-    fig, ax = plt.subplots()
-    ax.plot(consumption["consumption"], label="Verbrauch", color="b", marker="o", linewidth=2, markersize=6)
-    plt.legend()
-    st.pyplot(fig)
+    consumption = insights[["fdate", "consumption"]].dropna()
+    if consumption.shape[0] > 0:
+        fig, ax = plt.subplots()
+        ax.plot(consumption["consumption"], label="Verbrauch", color="b", marker="o", linewidth=2, markersize=6)
+        plt.legend()
+        st.pyplot(fig)
 
-price_per_unit = insights[["fdate", "price_per_unit"]].dropna()
-if price_per_unit.shape[0] > 0:
-    fig, ax = plt.subplots()
-    ax.plot(price_per_unit["price_per_unit"], label="Literpreis", color="r", marker="s", linewidth=2, markersize=6)
-    plt.legend()
-    st.pyplot(fig)
+    price_per_unit = insights[["fdate", "price_per_unit"]].dropna()
+    if price_per_unit.shape[0] > 0:
+        fig, ax = plt.subplots()
+        ax.plot(price_per_unit["price_per_unit"], label="Literpreis", color="r", marker="s", linewidth=2, markersize=6)
+        plt.legend()
+        st.pyplot(fig)
 
-distance_per_day = insights[["fdate", "distance_per_day"]].dropna()
-if distance_per_day.shape[0] > 0:
-    fig, ax = plt.subplots()
-    ax.plot(distance_per_day["distance_per_day"], label="mittlere Tagesdistanz", color="k", marker="p", linewidth=2, markersize=6)
-    plt.legend()
-    st.pyplot(fig)
+    distance_per_day = insights[["fdate", "distance_per_day"]].dropna()
+    if distance_per_day.shape[0] > 0:
+        fig, ax = plt.subplots()
+        ax.plot(distance_per_day["distance_per_day"], label="mittlere Tagesdistanz", color="k", marker="p", linewidth=2, markersize=6)
+        plt.legend()
+        st.pyplot(fig)
 
-st.dataframe(insights.iloc[::-1])
+with tab_data:
+    st.dataframe(insights.iloc[::-1])
